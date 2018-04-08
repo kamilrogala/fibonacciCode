b40602fd29c684c76081146b75422ec072f3ea4a
kamilrogala <contact@kamilrogala.it>
2018-04-08 23:09:29 +0200

    first commit, and maybe the last one?

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project: fibonacciCode
+
+Tiny script for generating fibonacci code- you can type how long it should be
---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector('button').addEventListener('click', () => {
+	var fibonacciEnd = document.getElementById('number').value;
+	var outpuElement = document.getElementById('output');
+	outpuElement.innerHTML = '';
+	
+	var fibonacci = [0, 1, 1];
+	for (var i = 3; i < fibonacciEnd; i++) {
+		fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+	}
+	for (var i = 0; i < fibonacciEnd; i++) {
+		outpuElement.innerHTML += '<li>' + fibonacci[i] + '</li>';
+	};
+});
